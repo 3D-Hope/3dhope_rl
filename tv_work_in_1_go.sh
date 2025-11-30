a52d571d037edb4d6da24f9c5e4d8469b2c2ab81
thenaivekid <neupane.ashok.9696@gmail.com>
2025-11-30 21:46:10 +0100

    config update

--- tv_work_in_1_go.sh
+++ tv_work_in_1_go.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=tv_work_in_1_go_flux_floor
 #SBATCH --partition=batch
-#SBATCH --gpus=h200:1
+#SBATCH --gpus=h200:8
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=20G
-#SBATCH --time=3-00:00:00
+#SBATCH --time=15:00:00
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
 
@@ -361,7 +361,7 @@ PYTHONPATH=. python -u main.py +name=tv_work_in_1_go_flux_floor \
     experiment.reset_lr_scheduler=True \
     experiment.training.lr=1e-6 \
     experiment.lr_scheduler.num_warmup_steps=250 \
-    algorithm.ddpo.batch_size=256 \
+    algorithm.ddpo.batch_size=192 \
     experiment.training.checkpointing.every_n_train_steps=500 \
     algorithm.num_additional_tokens_for_sampling=0 \
     algorithm.ddpo.n_timesteps_to_sample=100 \
